3762d9f355cb4742e7f84d93a90344bb0548ce5e
krishna kant <kk84singh@gmail.com>
2020-04-05 03:29:45 +0530

    AdditionData

---
+++ SitComTech.Model/DataObject/AdditionalInfo.cs
@@ -0,0 +1,19 @@
+﻿using SitComTech.Model.Common;
+
+namespace SitComTech.Model.DataObject
+{
+    public class AdditionalInfo :BaseConfig
+    {
+        public long OwnerId { get; set; }
+        public bool? SuppliedDocs { get; set; }
+        public bool? AcceptedTermConditions { get; set; }
+
+        public bool? SubscribedNewsletter { get; set; }
+
+        public bool? IsOnline { get; set; }
+        public string Description { get; set; }
+
+        public string PromoCode { get; set; }
+        public virtual User UserTable { get; set; }
+    }
+}
---
+++ SitComTech.Model/DataObject/Email.cs
@@ -0,0 +1,28 @@
+﻿using SitComTech.Model.Common;
+using System;
+
+namespace SitComTech.Model.DataObject
+{
+    public class Email :BaseConfig
+    {
+        public long OwnerId { get; set; }
+        
+        public string Subject { get; set; }
+
+        public Nullable<System.DateTime> SendDate { get; set; }
+        public string To { get; set; }
+
+        public string Bcc { get; set; }
+
+        public string Sender { get; set; }
+
+        public string StatusName { get; set; }
+
+        public bool? IsSend { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public string Body { get; set; }
+        public virtual User UserTable { get; set; }
+    }
+}
