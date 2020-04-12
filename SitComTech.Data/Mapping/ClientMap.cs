d9586796f88224002c318a40561ee231fe90ec97
krishna kant <kk84singh@gmail.com>
2020-04-12 13:07:04 +0530

    cleinttable

---
+++ SitComTech.Data/Mapping/ClientMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SitComTech.Model.Common;
+using SitComTech.Model.DataObject;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SitComTech.Data.Mapping
+{
+    public class ClientMap : EntityTypeConfiguration<Client>
+    {
+        public ClientMap()
+        {
+            HasKey(a => a.Id);
+            Property(a => a.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            HasRequired(a => a.UserTable).WithMany().HasForeignKey(x => x.OwnerId).WillCascadeOnDelete(false);
+        }
+
+    }
+}
