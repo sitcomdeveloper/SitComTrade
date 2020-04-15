d9ad5a4dac7ac002c2fe93e9c6bf121c52e60181
sitcomdeveloper43 <sitcomdeveloper43@gmail.com>
2020-04-15 11:34:57 +0530

    On id click get all information on info pg..Commit By Rohan

--- SitComTrade.NGWeb/src/app/clients_info/general-info/general-info.component.ts
+++ SitComTrade.NGWeb/src/app/clients_info/general-info/general-info.component.ts
@@ -13,15 +13,17 @@ export class GeneralInfoComponent implements OnInit {
   stk: any;
   editGeneralInfo = false;
   editInfo = false
-  userInfoForm: FormGroup
-  constructor(private _generalinfoservice: GeneralInfoService, private _router: Router, private _route: ActivatedRoute,private fb: FormBuilder) {this.edituserInfo(); }
+  userInfoForm: FormGroup;
+  Apptitle:any;
+  useraddinfo: any;
+  constructor(private _generalinfoservice: GeneralInfoService, private _router: Router, private _route: ActivatedRoute,private fb: FormBuilder) { }
 
   ngOnInit() {
-    this._route.params.subscribe(params => console.log(params));
+     this._route.params.subscribe(params => console.log(params));
     this._route.paramMap.subscribe(params => {
       this.stk = params.get("userid");
       if(this.stk === "1") {
-        
+
         this.editGeneralInfo = true;
         
         this.editInfo = false;
@@ -67,63 +69,70 @@ export class GeneralInfoComponent implements OnInit {
       lasttaskdayspast :[''],
       daysagoclientcreated :['']
     })
-    this.usersInfo();
+      this.Apptitle = JSON.parse(localStorage.getItem('project'));
+        console.log( 'aaaaa', this.Apptitle);
+this.userGenralinfo= this.Apptitle;
+   // this.usersInfo();
+   this.edituserInfo();
   }
-  usersInfo() {
+  // usersInfo() {
 
-    this._generalinfoservice.getUsersInfo().subscribe(res => {
-      this.userGenralinfo = res;
-      console.log('generalinfo', res);
-    });
-  }
+  //   this._generalinfoservice.getUsersInfo().subscribe(res => {
+  //     this.useraddinfo= res;
+  //     console.log('generalinfo', res);
+  //   });
+  // }
 edituserInfo() {
-  this._generalinfoservice.getUsersInfo().subscribe(res => {
-    this.userGenralinfo = res;
-    let date = this.userGenralinfo.CreatedAt.split('T');
+ // this._generalinfoservice.getUsersInfo().subscribe(res => {
+      this.Apptitle = JSON.parse(localStorage.getItem('project'));
+        console.log( 'aaaaa', this.Apptitle);
+        this.userGenralinfo= this.Apptitle;
+     this.useraddinfo = this.userGenralinfo;
+    let date = this.useraddinfo.CreatedDate.split('T');
     let userDate = date[0];
     this.userInfoForm.patchValue({
-      firstName: this.userGenralinfo.FirstName,
-      lastName: this.userGenralinfo.LastName,
-      email: this.userGenralinfo.Email,
-      phone: this.userGenralinfo.Phone,
-      mobile: this.userGenralinfo.Mobile,
-      secondemail: this.userGenralinfo.SecondEmail,
-      itemid: this.userGenralinfo.ItemId,
-      owner: this.userGenralinfo.OwnerId,
-      status: this.userGenralinfo.StatusName,
+      firstName: this.useraddinfo.FirstName,
+      lastName: this.useraddinfo.LastName,
+      email: this.useraddinfo.Email,
+      phone: this.useraddinfo.Phone,
+      mobile: this.useraddinfo.Mobile,
+      secondemail: this.useraddinfo.SecondEmail,
+      itemid: this.useraddinfo.ItemId,
+      owner: this.useraddinfo.OwnerId,
+      status: this.useraddinfo.StatusName,
       createddate: userDate,
       // lastcommentdate:: this.userGenralinfo.,
-      modifieddate: this.userGenralinfo.UpdatedAt,
+      modifieddate: this.useraddinfo.UpdatedAt,
       // conventionowner: this.userGenralinfo.
       // retentionowner: this.userGenralinfo.
       // citizenship: this.userGenralinfo.,
       // dob: this.userGenralinfo.,
-      ftd: this.userGenralinfo.Ftd,
+      ftd: this.useraddinfo.Ftd,
       // ftdate: this.userGenralinfo.,
-      enabled: this.userGenralinfo.Enabled,
+      enabled: this.useraddinfo.Enabled,
       // clienttime: this.userGenralinfo.,
       // lastlogindate: this.userGenralinfo.,
-      desk: this.userGenralinfo.Desk,
+      desk: this.useraddinfo.Desk,
       // hastasks: this.userGenralinfo.,
       // taskcreateddate: this.userGenralinfo.,
       // taskdate: this.userGenralinfo.,
-      assigneddate: this.userGenralinfo.AssignedDate,
+      assigneddate: this.useraddinfo.AssignedDate,
       // hasnotcompletedtasks: this.userGenralinfo.,
       // totaldeposits: this.userGenralinfo.,
       // ftdamount: this.userGenralinfo.,
       // totalwithdrawals: this.userGenralinfo.,
       // netdeposits: this.userGenralinfo.,
       // type: this.userGenralinfo.TypeName,
-      firstregistrationdate: this.userGenralinfo.FirstRegistrationDate,
+      firstregistrationdate: this.useraddinfo.FirstRegistrationDate,
       // registrationtype: this.userGenralinfo.,
-      lasttaskdayspast: this.userGenralinfo.LastTaskDaysPast,
-      daysagoclientcreated: this.userGenralinfo.DaysAgoClientCreated,
+      lasttaskdayspast: this.useraddinfo.LastTaskDaysPast,
+      daysagoclientcreated: this.useraddinfo.DaysAgoClientCreated,
       
      
       
 
 
     })
-  });
+  // });
 }
 }
