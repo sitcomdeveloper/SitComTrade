b8462c45da00a82eb6f83f91cf69afbf13716ba7
krishna kant <kk84singh@gmail.com>
2020-04-19 00:42:31 +0530

    addenum

---
+++ SitComTech.Model/Constants/Enums.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace SitComTech.Model.Constants
+{
+    public class EnumModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class EnumDescriptionModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+    public enum RegistrationTypeEnum
+    {
+        [Description("Direct")]
+        RegistrationType_Direct = 1,
+        [Description("Import")]
+        RegistrationType_Import = 2,
+        [Description("API")]
+        RegistrationType_API = 3,
+        [Description("Manual")]
+        RegistrationType_Manual = 4
+    }
+}
