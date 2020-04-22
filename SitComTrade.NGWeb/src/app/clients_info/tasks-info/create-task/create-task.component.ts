a62fe1eaca34b6eb8082d904f39ba961f5b83ca1
sitcomdeveloper43 <sitcomdeveloper43@gmail.com>
2020-04-22 12:32:59 +0530

    Rohan change in service

--- SitComTrade.NGWeb/src/app/clients_info/tasks-info/create-task/create-task.component.ts
+++ SitComTrade.NGWeb/src/app/clients_info/tasks-info/create-task/create-task.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TasksInfoService } from '../tasks-info.service';
 
 @Component({
   selector: 'app-create-task',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-task.component.css']
 })
 export class CreateTaskComponent implements OnInit {
+  getTasks: any;
+  getTaskTypeData: any;
+  getStatus: any;
+  getTaskStatusData: any;
 
-  constructor() { }
+  constructor(private taskInfoService: TasksInfoService) { }
 
   ngOnInit() {
+    this.taskType();
+    this.taskStatus();
+  }
+  taskType() {
+    this.taskInfoService.getTaskType(this.getTaskTypeData).subscribe(res => {
+      this.getTasks = res;
+      console.log('tasktype', res);
+    });
+  }
+  taskStatus() {
+    this.taskInfoService.getTaskStatus(this.getTaskStatusData).subscribe(res => {
+      this.getStatus = res;
+      console.log('taskstatus', res);
+    });
   }
 
 }
