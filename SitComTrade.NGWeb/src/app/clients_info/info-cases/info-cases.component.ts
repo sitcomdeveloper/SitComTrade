cdbe5fd301d24d810eefba141d4d8cbc68a6b87b
rupampriya <37929166+rupampriya@users.noreply.github.com>
2020-05-01 07:01:17 -0700

    add component

---
+++ SitComTrade.NGWeb/src/app/clients_info/info-cases/info-cases.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-cases',
+  templateUrl: './info-cases.component.html',
+  styleUrls: ['./info-cases.component.css']
+})
+export class InfoCasesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ SitComTrade.NGWeb/src/app/clients_info/info-documents/info-documents.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-documents',
+  templateUrl: './info-documents.component.html',
+  styleUrls: ['./info-documents.component.css']
+})
+export class InfoDocumentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
