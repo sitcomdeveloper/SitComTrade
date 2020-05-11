a9ce9e6439ddb3c8d6893c773b4f3c6ba5dbf9d0
sitcomdeveloper43 <sitcomdeveloper43@gmail.com>
2020-05-11 12:21:41 +0530

    Rohan additional-info update

--- SitComTrade.NGWeb/src/app/clients_info/additional-info/additional-info.component.ts
+++ SitComTrade.NGWeb/src/app/clients_info/additional-info/additional-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdditionalInfoService } from './additional-info.service';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 
 @Component({
   selector: 'app-additional-info',
@@ -12,8 +13,10 @@ export class AdditionalInfoComponent implements OnInit {
   additionalForm: FormGroup;
   AdditionalInfo = true;
   additionalInfoEdit = false;
+  getinsertInfo: any;
 
-  constructor(private additionalinfoservice: AdditionalInfoService, private fb: FormBuilder) {  }
+  constructor(private additionalinfoservice: AdditionalInfoService, private fb: FormBuilder, 
+              private spinnerService: Ng4LoadingSpinnerService) {  }
 
   ngOnInit() {
     this.additionalForm = this.fb.group({
@@ -30,41 +33,49 @@ export class AdditionalInfoComponent implements OnInit {
     this.additionalinfoservice.getAdditionalInfo().subscribe(res => {
       this.userAdditionalInfo = res;
       this.additionalForm.patchValue({
+        supplieddocs: this.userAdditionalInfo.SuppliedDocs,
+      acceptedtermsconditions: this.userAdditionalInfo.AcceptedTermConditions,
+      subscribednewsletter: this.userAdditionalInfo.SubscribedNewsletter,
+      isonline: this.userAdditionalInfo.IsOnline,
         description: this.userAdditionalInfo.Description,
         promocode: this.userAdditionalInfo.PromoCode
       });
       console.log('additionalinfo', res);
     });
   }
-  // editAdditionalInfo() {
-  //   this.additionalinfoservice.getAdditionalInfo().subscribe(res => {
-  //     this.userAdditionalInfo = res;
-  //     this.additionalForm.patchValue({
-  //       supplieddocs: this.userAdditionalInfo.SuppliedDocs,
-  //       acceptedtermsconditions: this.userAdditionalInfo.AcceptedTermConditions,
-  //       subscribednewsletter: this.userAdditionalInfo.SubscribedNewsletter,
-  //       isonline: this.userAdditionalInfo.IsOnline,
-  //       description: this.userAdditionalInfo.Description,
-  //       promocode: this.userAdditionalInfo.PromoCode
-  //     });
-  //   });
-  // }
   // div show hide
   // pencil
   showhide() {
     this.AdditionalInfo = false;
-  this.additionalInfoEdit = true;
+    this.additionalInfoEdit = true;
   }
-  // apply
+  // apply.insert additional-info
   saveShowHide() {
+    const obj = {
+      AcceptedTermConditions: this.additionalForm.value.acceptedtermsconditions,
+      Description: this.additionalForm.value.description,
+      OwnerId: this.userAdditionalInfo.OwnerId,
+      IsOnline: this.additionalForm.value.isonline,
+      PromoCode: this.additionalForm.value.promocode,
+      SubscribedNewsletter: this.additionalForm.value.subscribednewsletter,
+      SuppliedDocs: this.additionalForm.value.supplieddocs,
+      Id: this.userAdditionalInfo.Id,
+    };
+    this.additionalinfoservice.insertAdditionalInfo(obj).subscribe(res => {
+      this.getinsertInfo = res;
+      this.spinnerService.show();
+      this.additionalInfo();
+      console.log('getinsertInfo', res);
+    });
     this.AdditionalInfo = true;
-  this.additionalInfoEdit = false;
+    this.additionalInfoEdit = false;
   }
   // cancel
   cancel() {
     this.AdditionalInfo = true;
-  this.additionalInfoEdit = false;
+    this.additionalInfoEdit = false;
   }
+
 
 }
 
--- SitComTrade.NGWeb/src/app/clients_info/additional-info/additional-info.service.ts
+++ SitComTrade.NGWeb/src/app/clients_info/additional-info/additional-info.service.ts
@@ -3,15 +3,19 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 
-const API_URL=environment.API_URL;
+const API_URL = environment.API_URL;
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdditionalInfoService {
 
-  constructor(private http:HttpClient) { }
-  getAdditionalInfo(): Observable<any>{
-    return this.http.get<any>(API_URL + "Client/GetAdditionalInfoByOwnerId/3");
+  constructor(private http: HttpClient) { }
+  getAdditionalInfo(): Observable<any> {
+    return this.http.get<any>(API_URL + 'Client/GetAdditionalInfoByOwnerId/3');
+  }
+  // insert-update
+  insertAdditionalInfo(obj: any): Observable<any> {
+    return this.http.post<any>(API_URL + 'Client/InsertUpdateAdditionalInfo', obj);
   }
 }
