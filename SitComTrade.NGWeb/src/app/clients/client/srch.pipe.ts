ae4cd5d14abddf6e015f3f7862267428a0688de3
sitcomdeveloper43 <sitcomdeveloper43@gmail.com>
2020-05-15 13:12:09 +0530

    Rohan

--- SitComTrade.NGWeb/src/app/clients/client/srch.pipe.ts
+++ SitComTrade.NGWeb/src/app/clients/client/srch.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SrchPipe implements PipeTransform {
 
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any, args?: any): any {
+    if(!args){
+      return value;
+    }
+    return value.filter((val)=>{
+      let rVal=(val.FirstName.toLocaleLowerCase().includes(args));
+      return rVal;
+    })
+    
   }
 
 }
+
+
+// || (val.venue.toLocaleLowerCase().includes(args)) || (val.timing.toString().includes(args)) || (val.address.toLocaleLowerCase().includes(args)
