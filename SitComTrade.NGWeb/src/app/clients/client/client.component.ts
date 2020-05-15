b0675a1030ac3afc22a5e301989ea92c5ed37211
rupampriya <37929166+rupampriya@users.noreply.github.com>
2020-05-15 02:00:34 -0700

    model change

--- SitComTrade.NGWeb/src/app/clients/client/client.component.ts
+++ SitComTrade.NGWeb/src/app/clients/client/client.component.ts
@@ -152,7 +152,7 @@ bindLoginData: any;
       rmvClient: 'rmvClient'
     };
     // tslint:disable-next-line: max-line-length
-    this.bsModalRef = this.modalService.show(DeleteComponent, Object.assign({ backdrop: 'static', show: true }, { class: 'modal-lg', initialState }));
+    this.bsModalRef = this.modalService.show(DeleteComponent, Object.assign({ backdrop: 'static', show: true }, { class: 'modal930', initialState }));
     this.bsModalRef.content.closeBtnName = 'Cancel';
     this.bsModalRef.content.clddata.subscribe(data => {
       this.userDetails();
