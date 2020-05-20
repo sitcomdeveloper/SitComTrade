7d81b4badeed7cdc246c0505b7339340957e15c1
sitcomdeveloper43 <sitcomdeveloper43@gmail.com>
2020-05-20 17:39:38 +0530

    Rohan crm users

--- SitComTrade.NGWeb/src/app/settings/crmusers/crmusers.component.ts
+++ SitComTrade.NGWeb/src/app/settings/crmusers/crmusers.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-crmusers',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crmusers.component.css']
 })
 export class CrmusersComponent implements OnInit {
-
-  constructor() { }
+  getLoginDetails: any;
+  bindLoginData: any;
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) {}
 
   ngOnInit() {
+     // code for receiving login details and bind to header at place of name
+     this.getLoginDetails = JSON.parse(localStorage.getItem('project'));
+     this.bindLoginData = this.getLoginDetails;
+  }
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
 }
