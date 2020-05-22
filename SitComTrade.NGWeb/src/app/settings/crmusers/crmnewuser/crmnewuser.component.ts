657f19bd837e8d9a97d7a2d48f0172a665fb9916
rupampriya <37929166+rupampriya@users.noreply.github.com>
2020-05-22 12:02:03 -0700

    popup of new user

---
+++ SitComTrade.NGWeb/src/app/settings/crmusers/crmnewuser/crmnewuser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crmnewuser',
+  templateUrl: './crmnewuser.component.html',
+  styleUrls: ['./crmnewuser.component.css']
+})
+export class CrmnewuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
