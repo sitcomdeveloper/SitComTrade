84fb001d1b81793ef1b33f8f54ea36640343b5e1
Rohan Gupta <sitcomdeveloper43@gmail.com>
2020-06-11 15:51:51 -0700

    Rohan

--- SitComTrade.NGWeb/src/app/clients_info/email/email.component.ts
+++ SitComTrade.NGWeb/src/app/clients_info/email/email.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActcrtaccComponent } from 'src/app/clients-info/actcrtacc/actcrtacc.component';
+import { ModalDirective, BsModalRef, BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { ActivatedRoute } from '@angular/router';
+import { GeneralInfoService } from '../general-info/general-info.service';
 
 @Component({
   selector: 'app-email',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email.component.css']
 })
 export class EmailComponent implements OnInit {
+  detail: number;
+  userGenralinfo: any;
 
-  constructor() { }
-
+  constructor(private modalService: BsModalService,private _route: ActivatedRoute,private _generalinfoservice:GeneralInfoService) { }
+  bsModalRef: BsModalRef;
   ngOnInit() {
+    // For jump to specific clients.see below method "sendData"
+    const details = +this._route.snapshot.paramMap.get('selectedItem');
+    this.detail = details;
+    // API of general section use for showing name of selected client
+    // this.spinnerService.show();
+    
+    this._generalinfoservice.getUsersInfo(details).subscribe(res => {
+      this.userGenralinfo = res;
+    });
   }
-
+  sendemail(Ide) {
+    const initialState = {
+      title: 'SEND EMAIL',
+      sendemail: 'sendemail',
+      // get Id for showing email on popup
+      detailss: Ide
+    };
+    // tslint:disable-next-line: max-line-length
+    this.bsModalRef = this.modalService.show(ActcrtaccComponent, Object.assign({ backdrop: 'static', show: true }, { class: 'modal750', initialState }));
+    this.bsModalRef.content.closeBtnName = 'Cancel';
+  }
 }
