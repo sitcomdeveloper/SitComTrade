7238f28fce2bad5c6f7cb58b096e41dfe16356a1
Rohan Gupta <sitcomdeveloper43@gmail.com>
2020-06-17 16:20:59 -0700

    Rohan

--- SitComTrade.NGWeb/src/app/clients_info/reg-his/reg-his.component.ts
+++ SitComTrade.NGWeb/src/app/clients_info/reg-his/reg-his.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery' 
+import { ActivatedRoute } from '@angular/router';
+import { GeneralInfoService } from '../general-info/general-info.service';
 @Component({
   selector: 'app-reg-his',
   templateUrl: './reg-his.component.html',
   styleUrls: ['./reg-his.component.css']
 })
 export class RegHisComponent implements OnInit {
+  userGenralinfo: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private generalinfoservice: GeneralInfoService) { }
 
   ngOnInit() {
- 
-  $(document).ready(function () {
+ $(document).ready(function () {
     $("#reg").click(function () {
       $(".showrcrdreg").toggle();
     });
   });
-}
+   // receiving data from client page for general-info
+   const details = +this.route.snapshot.paramMap.get('selectedItem');
+   this.generalinfoservice.getUsersInfo(details).subscribe(res => {
+     this.userGenralinfo = res;
+})
+  }
 }
