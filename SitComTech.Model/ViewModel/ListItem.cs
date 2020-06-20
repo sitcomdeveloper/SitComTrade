7e5093c3e3e55b9c14c6acde0a3c868baf2dc082
krishna kant <kk84singh@gmail.com>
2020-06-20 16:46:42 +0530

    send selected email

---
+++ SitComTech.Model/ViewModel/ListItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitComTech.Model.ViewModel
+{
+    public class ListItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool Selected { get; set; }
+        public string Other { get; set; }
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public long? CommodityId { get; set; }
+        public string Utility { get; set; }
+        public long? StateId { get; set; }
+        public long? CustomerId { get; set; }
+        public DateTime Date { get; set; }
+        public long UserLoginLevelId { get; set; }
+        public long? ReportToId { get; set; }
+    }
+}
