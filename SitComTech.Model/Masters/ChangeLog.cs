289a676f8183fa63a412a8f5f6fdeae9adb78d3e
ravikanthudda <ravikanthudda@gmail.com>
2020-06-21 15:01:30 +0530

    History table is created and logic for history is written and one gethistory api is also written.

---
+++ SitComTech.Model/Masters/ChangeLog.cs
@@ -0,0 +1,25 @@
+﻿using SitComTech.Framework.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitComTech.Model.Masters
+{
+    public class ChangeLog: BaseEntityState
+    {
+        public int Id { get; set; }
+        public string EntityName { get; set; }
+        public string PropertyName { get; set; }
+        public string PrimaryKeyValue { get; set; }
+        public string OldValue { get; set; }
+        public string NewValue { get; set; }
+        public Nullable<DateTime> DateChanged { get; set; }
+        public Nullable<int> ChangedBy { get; set; }
+        public string ChangedByName { get; set; }
+        public string IpAddress { get; set; }
+        public int OwnerId { get; set; }
+    }
+
+}
