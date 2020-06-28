3d748da587d8f5baf5d02e2825f98fd46be2442c
krishna kant <kk84singh@gmail.com>
2020-06-28 10:39:14 +0530

    workflow code

---
+++ SitComTech.API/Controllers/WorkFlowController.cs
@@ -0,0 +1,76 @@
+﻿using SitComTech.Core.Interface;
+using SitComTech.Framework.UnitOfWork;
+using SitComTech.Model.DataObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace SitComTech.API.Controllers
+{
+    [RoutePrefix("api/WorkFlow")]
+    public class WorkFlowController : ApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        private IWorkFlowService _workflowService;
+        public WorkFlowController(IWorkFlowService workflowService, IUnitOfWork unitOfWork)
+        {
+            this._workflowService = workflowService;
+            this._unitOfWork = unitOfWork;
+
+        }
+
+        [HttpPost]
+        [Route("InsertWorkFlow")]
+        public void InsertWorkFlow(WorkFlow entity)
+        {
+            _workflowService.InsertWorkFlow(entity);
+        }
+
+        [HttpPost]
+        [Route("GetWorkFlowDetailById/{id}")]
+        public WorkFlow GetWorkFlowDetailById(long id)
+        {
+            return _workflowService.GetWorkFlowById(id);
+        }
+
+        [HttpPost]
+        [Route("GetAllWorkFlows")]
+        public List<WorkFlow> GetAllWorkFlows()
+        {
+            return _workflowService.GetWorkFlowList();
+        }
+
+        [HttpPost]
+        [Route("UpdateWorkFlow")]
+        public void UpdateWorkFlow(WorkFlow groupVM)
+        {
+            _workflowService.UpdateWorkFlow(groupVM);
+        }
+
+
+
+        [HttpPost]
+        [Route("DeleteWorkFlowById/{id}")]
+        public bool DeleteWorkFlowById(long id)
+        {
+            try
+            {
+                if (id != 0)
+                {
+                    return _workflowService.DeleteWorkFlowById(id);
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }    
+}
---
+++ SitComTech.Core/Interface/IWorkFlowService.cs
@@ -0,0 +1,16 @@
+﻿using SitComTech.Data.Interface;
+using SitComTech.Framework.Services;
+using SitComTech.Model.DataObject;
+using System.Collections.Generic;
+
+namespace SitComTech.Core.Interface
+{    
+    public interface IWorkFlowService : IService<WorkFlow>
+    {
+        List<WorkFlow> GetWorkFlowList();
+        WorkFlow GetWorkFlowById(object Id);
+        void InsertWorkFlow(WorkFlow entity);
+        void UpdateWorkFlow(WorkFlow entity);
+        bool DeleteWorkFlowById(long workFlowId);
+    }
+}
