fae81a89516abcb7a3c35f2d06c75c6c156b0062
krishna kant <kk84singh@gmail.com>
2020-07-04 03:41:16 +0530

    sender setting code

---
+++ SitComTech.Core/Interface/ISenderSettingService.cs
@@ -0,0 +1,17 @@
+﻿using SitComTech.Model.Masters;
+using SitComTech.Framework.Services;
+using SitComTech.Model.DataObject;
+using System.Collections.Generic;
+
+
+namespace SitComTech.Core.Interface
+{    
+    public interface ISenderSettingService : IService<SenderSetting>
+    {
+        List<SenderSetting> GetSenderSettingList();
+        SenderSetting GetSenderSettingById(object Id);
+        void InsertSenderSetting(SenderSetting entity);
+        void UpdateSenderSetting(SenderSetting entity);
+        bool DeleteSenderSettingById(long senderSettingId);
+    }
+}
