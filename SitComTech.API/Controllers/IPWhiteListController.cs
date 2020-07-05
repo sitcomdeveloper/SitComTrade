b81a8bcb477023b485ecf172af0fe83090b113c0
krishna kant <kk84singh@gmail.com>
2020-07-06 00:54:08 +0530

    add code for ip address

---
+++ SitComTech.API/Controllers/IPWhiteListController.cs
@@ -0,0 +1,76 @@
+﻿using SitComTech.Core.Interface;
+using SitComTech.Framework.UnitOfWork;
+using SitComTech.Model.DataObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace SitComTech.API.Controllers
+{    
+    [RoutePrefix("api/IPWhiteList")]
+    public class IPWhiteListController : ApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        private IIPWhiteListService _IPWhiteListService;
+        public IPWhiteListController(IIPWhiteListService IPWhiteListService, IUnitOfWork unitOfWork)
+        {
+            this._IPWhiteListService = IPWhiteListService;
+            this._unitOfWork = unitOfWork;
+
+        }
+
+        [HttpPost]
+        [Route("InsertIPWhiteList")]
+        public void InsertIPWhiteList(IPWhiteList entity)
+        {
+            _IPWhiteListService.InsertIPWhiteList(entity);
+        }
+
+        [HttpPost]
+        [Route("GetIPWhiteListDetailById/{id}")]
+        public IPWhiteList GetIPWhiteListDetailById(long id)
+        {
+            return _IPWhiteListService.GetIPWhiteListById(id);
+        }
+
+        [HttpPost]
+        [Route("GetAllIPWhiteLists")]
+        public List<IPWhiteList> GetAllIPWhiteLists()
+        {
+            return _IPWhiteListService.GetIPWhiteList();
+        }
+
+        [HttpPost]
+        [Route("UpdateIPWhiteList")]
+        public void UpdateIPWhiteList(IPWhiteList groupVM)
+        {
+            _IPWhiteListService.UpdateIPWhiteList(groupVM);
+        }
+
+
+
+        [HttpPost]
+        [Route("DeleteIPWhiteListById/{id}")]
+        public bool DeleteIPWhiteListById(long id)
+        {
+            try
+            {
+                if (id != 0)
+                {
+                    return _IPWhiteListService.DeleteIPWhiteListById(id);
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
