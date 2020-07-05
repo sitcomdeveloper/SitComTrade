07776f4de82248e8923ac255d2d55db605947e08
krishna kant <kk84singh@gmail.com>
2020-07-06 00:54:08 +0530

    add code for ip address

---
+++ SitComTech.Domain/Services/IPWhiteListService.cs
@@ -0,0 +1,104 @@
+﻿using SitComTech.Core.Interface;
+using SitComTech.Data.Interface;
+using SitComTech.Framework.Repositories;
+using SitComTech.Framework.Services;
+using SitComTech.Framework.UnitOfWork;
+using SitComTech.Model.DataObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SitComTech.Domain.Services
+{    
+    public class IPWhiteListService : Service<IPWhiteList>, IIPWhiteListService
+    {
+        private IGenericRepository<IPWhiteList> _repository;
+        private IUnitOfWork _unitOfWork;
+        public IPWhiteListService(IGenericRepository<IPWhiteList> repository, IUnitOfWork unitOfWork)
+            : base(repository)
+        {
+            this._repository = repository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public IPWhiteList GetIPWhiteListById(object Id)
+        {
+            if ((long)Id == 0)
+                return null;
+            IPWhiteList vinstr = _repository.Queryable().FirstOrDefault(x => x.Id == (long)Id && x.Active && !x.Deleted);
+            return vinstr;
+        }
+
+        public List<IPWhiteList> GetIPWhiteList()
+        {
+            return _repository.Queryable().Where(x => x.Active && !x.Deleted).ToList();
+        }
+
+        public void InsertIPWhiteList(IPWhiteList entity)
+        {
+            try
+            {
+                IPWhiteList IPWhiteList = new IPWhiteList
+                {
+                    Active = true,
+                    Deleted = false,
+                    CreatedAt = DateTime.Now,
+                    CreatedBy = 0,
+                    CreatedByName = "",
+                    IPAddress = entity.IPAddress,
+                    Description = entity.Description,
+                    UserId = entity.UserId
+                   
+                };
+                _repository.Insert(IPWhiteList);
+                _unitOfWork.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void UpdateIPWhiteList(IPWhiteList entity)
+        {
+            IPWhiteList _instrument = _repository.Queryable().FirstOrDefault(x => x.Id == entity.Id);
+            if (_instrument != null)
+            {
+                _instrument.UpdatedAt = DateTime.Now;
+                _instrument.IPAddress = entity.IPAddress;
+                _instrument.Description = entity.Description;
+                _instrument.UserId = entity.UserId;               
+                _repository.Update(_instrument);
+                _unitOfWork.SaveChanges();
+            }
+            if (entity == null || _instrument == null)
+                throw new ArgumentNullException("IPWhiteList");
+        }
+        public void DeleteIPWhiteList(IPWhiteList entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException("IPWhiteList");
+            _repository.Delete(entity);
+            _unitOfWork.SaveChanges();
+        }
+
+        public bool DeleteIPWhiteListById(long IPWhiteListId)
+        {
+            try
+            {
+                if (IPWhiteListId != 0)
+                {
+
+                    IPWhiteList _IPWhiteList = base.Queryable().Where(x => x.Active && !x.Deleted && x.Id == IPWhiteListId).FirstOrDefault();
+                    if (_IPWhiteList != null)
+                        DeleteIPWhiteList(_IPWhiteList);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ SitComTech.Model/DataObject/IPWhiteList.cs
@@ -0,0 +1,12 @@
+﻿using SitComTech.Framework.DataContext;
+using System;
+
+namespace SitComTech.Model.DataObject
+{  
+    public class IPWhiteList : BaseEntity
+    {
+        public Nullable<long> UserId { get; set; }
+        public string IPAddress { get; set; }
+        public string Description { get; set; }
+    }
+}
