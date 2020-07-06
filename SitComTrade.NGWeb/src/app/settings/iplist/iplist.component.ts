f4255364cf462795b3905f03136aa45d4c1d94aa
Rohan Gupta <sitcomdeveloper43@gmail.com>
2020-07-06 16:11:11 +0530

    Rohan get ip and crt ip

--- SitComTrade.NGWeb/src/app/settings/iplist/iplist.component.ts
+++ SitComTrade.NGWeb/src/app/settings/iplist/iplist.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../settings.service';
+import { IpDTO } from '../settingsDTO';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-iplist',
@@ -9,16 +11,42 @@ import { SettingsService } from '../settings.service';
 export class IplistComponent implements OnInit {
   getallIp: any;
   IP: any;
-
-  constructor(private settingsService: SettingsService) { }
+  crtnewIp: IpDTO;
+  newip: IpDTO;
+  IPForm: FormGroup
+  getLoginDetails: any;
+  bindLoginData: any;
+  constructor(private settingsService: SettingsService, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.IPForm = this.fb.group({
+      ip: [''],
+      description: ['']
+    })
+    // code for receiving login details and bind to header at place of name
+    this.getLoginDetails = JSON.parse(window.sessionStorage.getItem('username'));
+    this.bindLoginData = this.getLoginDetails;
     this.getallIPs();
   }
 getallIPs() {
   this.settingsService.getIP(this.getallIp).subscribe(fetchIp => {
-    this.IP = fetchIp;
+    this.IP = fetchIp.reverse();
     console.log('IP',fetchIp);
   })
+}
+// create new ip
+createnewIP() {
+  this.crtnewIp = {
+    Id: '',
+    IPAddress: this.IPForm.value.ip,
+    Description: this.IPForm.value.description,
+    UserId: this.bindLoginData.UserId
+  }
+this.settingsService.crtIp(this.crtnewIp).subscribe(getnewip => {
+  this.newip = getnewip;
+  this.IPForm.reset();
+  this.getallIPs();
+  console.log('newip',getnewip);
+})
 }
 }
