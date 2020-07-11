5757d2fd431a932a531a1546c71aa130d3c0cc25
krishna kant <kk84singh@gmail.com>
2020-07-11 22:45:12 +0530

    error log

---
+++ SitComTech.Core/Interface/IExceptionLoggerService.cs
@@ -0,0 +1,12 @@
+﻿using SitComTech.Data.Interface;
+using SitComTech.Framework.Services;
+using SitComTech.Model.Masters;
+using System.Collections.Generic;
+
+namespace SitComTech.Core.Interface
+{   
+    public interface IExceptionLoggerService : IService<ExceptionLogger>
+    { 
+        void InsertExceptionLogger(string desc, string clientname);        
+    }
+}
