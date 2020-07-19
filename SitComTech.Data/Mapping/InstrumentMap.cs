42a2796ae42fea3f4a4741fa02392701534743bc
krishna kant <kk84singh@gmail.com>
2020-07-19 23:54:23 +0530

    instrument modification

--- SitComTech.Data/Mapping/InstrumentMap.cs
+++ SitComTech.Data/Mapping/InstrumentMap.cs
@@ -12,4 +12,61 @@ namespace SitComTech.Data.Mapping
             Property(r => r.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
         }
     }
+
+    public class SpreadTypeMap : EntityTypeConfiguration<SpreadType>
+    {
+        public SpreadTypeMap()
+        {
+            HasKey(r => r.Id);
+            Property(r => r.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+    public class CalculationModeMap : EntityTypeConfiguration<CalculationMode>
+    {
+        public CalculationModeMap()
+        {
+            HasKey(r => r.Id);
+            Property(r => r.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+    public class SymbolGroupMap : EntityTypeConfiguration<SymbolGroup>
+    {
+        public SymbolGroupMap()
+        {
+            HasKey(r => r.Id);
+            Property(r => r.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+    public class SwapTypeMap : EntityTypeConfiguration<SwapType>
+    {
+        public SwapTypeMap()
+        {
+            HasKey(r => r.Id);
+            Property(r => r.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+    public class TradingHourMap : EntityTypeConfiguration<TradingHour>
+    {
+        public TradingHourMap()
+        {
+            HasKey(r => r.Id);
+            Property(r => r.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+    public class UnitMap : EntityTypeConfiguration<Unit>
+    {
+        public UnitMap()
+        {
+            HasKey(r => r.Id);
+            Property(r => r.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
+    public class DaySwapMap : EntityTypeConfiguration<DaySwap>
+    {
+        public DaySwapMap()
+        {
+            HasKey(r => r.Id);
+            Property(r => r.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        }
+    }
 }
