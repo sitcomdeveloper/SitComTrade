d6102d5a7687a7a39e506436439878cec226c533
krishna kant <kk84singh@gmail.com>
2020-07-27 01:38:50 +0530

    trade acc

---
+++ SitComTech.Core/Interface/ITradeAccountService.cs
@@ -0,0 +1,17 @@
+﻿using SitComTech.Data.Interface;
+using SitComTech.Framework.Services;
+using SitComTech.Model.DataObject;
+using System.Collections.Generic;
+
+namespace SitComTech.Core.Interface
+{
+    
+    public interface ITradeAccountService : IService<TradeAccount>
+    {
+        List<TradeAccount> GetTradeAccountList();
+        TradeAccount GetTradeAccountById(object Id);
+        void CreateTradeAccount(CreateTradeAccountVM entity);
+        void UpdateTradeAccount(TradeAccount entity);
+        bool DeleteTradeAccountById(long TradeAccountId);
+    }
+}
