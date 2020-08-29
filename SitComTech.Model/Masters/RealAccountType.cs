83b006a5e2760752ecdd08e8bb7475281013ce0c
krishna kant <kk84singh@gmail.com>
2020-08-29 20:11:44 +0530

    realacc

---
+++ SitComTech.Model/Masters/RealAccountType.cs
@@ -0,0 +1,9 @@
+﻿using SitComTech.Framework.DataContext;
+
+namespace SitComTech.Model.Masters
+{    
+    public class RealAccountType : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
